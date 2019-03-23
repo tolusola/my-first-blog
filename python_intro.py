a54e76fa0327f4a8a38ba04b0e44b1faf3eff4b3
Tosin <tosin.d.olusola@gmail.com>
2019-03-23 16:54:14 -0400

    Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,27 @@
+prit('Hello, Django girls')
+if 3>2:
+    print('It works!')
+if 5>2:
+    print('5 is indeed greater than 2')
+else:
+    print('5 is not greater than 2')
+name = 'Sonja'
+if name == 'Ola':
+    print('Hey Ola')
+elif name == 'Sonja':
+    print('Hey Sonja')
+else:
+    print('Hey anonymous!')
+volume = 57
+if volume < 20:
+    print("it's kinda quite.")
+elif 20 <= volume < 40:
+    print("It's nice for background music")
+elif 40 <= volume <= 60:
+    print("Perfect, I can hear all the details")
+if volume < 80:
+    print("Nice for parties")
+elif 80 <= volume < 100:
+    print("A bit loud!")
+else:
+    print("My ears are hurting! :(")
